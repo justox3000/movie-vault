6901034292dffde7223c880a843228c65e16e731
Justus Lambrecht <justuslambrecht@icloud.com>
2023-11-17 08:06:50 +0100

    redux

---
+++ src/redux/search.js
@@ -0,0 +1,39 @@
+import createsSlice from '@reduxjs/toolkit';
+
+const initialState = {
+  results: [],
+  totalResults: 0,
+  page: 0,
+  totalPages: 0,
+  isFetching: false
+};
+
+const searchSlice = createsSlice({
+  name: 'searchSlice',
+  initialState,
+  reducers: {
+    searchMovies: (state) => {
+      return {
+        ...state,
+        isFetching: true
+      };
+    },
+    fetchedSearchMovies: (state, action) => {
+      return {
+        ...state,
+        isFetching: false,
+        results: action.payload.results,
+        totalResults: action.payload.total_results,
+        page: action.payload.page,
+        totalPages: action.payload.total_pages
+      };
+    },
+    resetState: (state) => {
+      return initialState
+    }
+  }
+});
+
+export const { searchMovies, fetchedSearchMovies, resetState} = searchSlice.actions;
+
+export default searchSlice.reducers;
