9186a8c99752c5423ed51c4753a33beb730f5ad1
Justus Lambrecht <justuslambrecht@icloud.com>
2023-11-17 07:49:17 +0100

    added searchbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,8 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { Link } from "react-router-dom";
 import logoImg from '../logo.png'
+import { styled } from '@mui/system';
+import SearchMoviesSuggestion from "../containers/SearchMoviesSuggestion";
 
 const darkTheme = createTheme({
   palette: {
@@ -10,15 +12,36 @@ const darkTheme = createTheme({
   }
 })
 
+const Img = styled('img')({
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  display: 'block',
+  width: 500,
+  maxwidth: '100%',
+
+})
+
+const LayoutWrapper = styled('div')(({theme}) => ({
+  margin: 24,
+  width: 'auto',
+  [theme.breakpoints.up("lg")]: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: theme.breakpoints.values.lg,
+  }
+
+}))
+
 const Layout = ({children}) => {
   return(
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <div>
+      <LayoutWrapper>
         <Link to="/">
-          <img src={logoImg} alt="movieVault Logo"/>
+          <Img src={logoImg} alt="movieVault Logo"/>
         </Link>
-      </div>
+        <SearchMoviesSuggestion />
+      </LayoutWrapper>
       {children}
     </ThemeProvider>
   );
