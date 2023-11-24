b457e3ea1b6a9ee68773557d7703031be9600f39
Justus Lambrecht <justuslambrecht@icloud.com>
2023-11-24 12:00:51 +0100

    api

--- src/lib/api.js
+++ src/lib/api.js
@@ -8,7 +8,7 @@ export default class TheMovieDbApi {
 
   searchMovies = async (query) => {
     const response = await fetch(
-    `${this.apiBaseUrl}/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${query}`
+    `${this.apiBaseUrl}/search/movie?api_key=${this.apiKey}&query=${query}`
     );
 
     return response.json();
@@ -16,7 +16,7 @@ export default class TheMovieDbApi {
 
   getGenres = async () => {
     const response = await fetch(
-    `${this.apiBaseUrl}/genre/movie?api_key=${process.env.REACT_APP_API_KEY}`
+    `${this.apiBaseUrl}/genre/movie?api_key=${this.apiKey}`
     );
 
     return response.json();
